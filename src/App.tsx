a4f16914fbafc15bdf02322e44606bd4f7a70628
Official-Nakul <124872597+Official-Nakul@users.noreply.github.com>
2023-11-11 02:32:40 +0530

    base version of AgeCalculator

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import "./App.css";
+import arrow from "../public/images/icon-arrow.svg";
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="userInput">
+          <div className="day">
+            <p>DAY</p>
+            <input type="text" placeholder="DD" className="dayIn" />
+          </div>
+          <div className="month">
+            <p>MONTH</p>
+            <input type="text" placeholder="MM" className="monthIn" />
+          </div>
+          <div className="year">
+            <p>YEAR</p>
+            <input type="text" placeholder="YYYY" className="yearIn" />
+          </div>
+        </div>
+        <div className="btnArea">
+          <hr />
+          <div className="btn">
+            <img src={arrow} alt="" />
+          </div>
+        </div>
+        <div className="outputArea">
+          <div className="outputtxt">
+            <p className="calOutput">--</p>
+            <p>years</p>
+          </div>
+          <div className="outputtxt">
+            <p className="calOutput">--</p>
+            <p>months</p>
+          </div>
+          <div className="outputtxt">
+            <p className="calOutput">--</p>
+            <p>days</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
