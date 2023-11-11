9d8576dabaf1b46b69859cb7058de5a9a17be6e0
Official-Nakul <124872597+Official-Nakul@users.noreply.github.com>
2023-11-11 16:31:24 +0530

    Some minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,31 @@ function App() {
         <div className="userInput">
           <div className="day">
             <p>DAY</p>
-            <input type="text" placeholder="DD" className="dayIn" />
+            <input
+              type="text"
+              placeholder="DD"
+              className="dayIn"
+              maxLength={2}
+            />
           </div>
           <div className="month">
             <p>MONTH</p>
-            <input type="text" placeholder="MM" className="monthIn" />
+            <input
+              type="text"
+              placeholder="MM"
+              className="monthIn"
+              maxLength={2}
+            />
           </div>
           <div className="year">
             <p>YEAR</p>
-            <input type="text" placeholder="YYYY" className="yearIn" />
+            <input
+              type="text"
+              placeholder="YYYY"
+              className="yearIn"
+              maxLength={4}
+              minLength={4}
+            />
           </div>
         </div>
         <div className="btnArea">
